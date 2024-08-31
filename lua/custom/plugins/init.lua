9a298160b42d7d7ee125e7c43b3cf4e0f1654413
Tomasz Kowalski <113097647+tmkkow@users.noreply.github.com>
2024-08-31 09:21:40 +0200

    basic set up

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,25 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- Oil file menager
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('oil').setup()
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent direcI:tory' })
+    end,
+  },
+  {
+    'savq/melange-nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'melange'
+      vim.opt.termguicolors = true
+    end,
+  },
+}
